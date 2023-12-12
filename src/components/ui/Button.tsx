c8a6f3237f55a77fd558480e0113047e2e85b0e5
Nikola Kuruc <nikolakrc5@gmail.com>
2023-12-12 13:30:56 +0100

    Button componet modified

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,16 +1,12 @@
 import PropTypes from 'prop-types';
 import { ComponentProps } from 'react';
 
-function Button({ children, ...props }: ComponentProps<'button'>) {
-  return (
-    <button
-      className="border-2 border-blue-700 bg-blue-700 text-white py-1 w-20 rounded-sm transition ease-in-out delay-550 hover:bg-white hover:text-blue-700 font-semibold mb-2"
-      {...props}
-    >
-      {children}
-    </button>
-  );
-}
+const Button: React.FC<ComponentProps<'button'>> = (props) => (
+  <button
+    className="border-2 border-blue-700 bg-blue-700 text-white py-1 w-20 rounded-sm transition ease-in-out delay-550 hover:bg-white hover:text-blue-700 font-semibold mb-2"
+    {...props}
+  />
+);
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
